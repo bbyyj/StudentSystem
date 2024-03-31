557b70876d35fa59065e5d998f8c4ebf3993f76f
bingfeng613 <2546414896@qq.com>
2024-03-31 22:44:34 +0800

    综测审核- 初步

--- frontend/src/Views/compAssReview.vue
+++ frontend/src/Views/compAssReview.vue
@@ -1,14 +1,82 @@
 <template>
-    <h1> 综测审核页面</h1>
+    <div>
+        <el-form inline class="search-form" @submit.native.prevent="fetchData">
+            <el-form-item label="学号">
+                <el-input v-model="searchTerms.a" placeholder="请输入A"></el-input>
+            </el-form-item>
+            <el-form-item label="姓名">
+                <el-input v-model="searchTerms.b" placeholder="请输入B"></el-input>
+            </el-form-item>
+            <el-form-item label="班级">
+                <el-input v-model="searchTerms.c" placeholder="请输入C"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="fetchData">搜索</el-button>
+            </el-form-item>
+        </el-form>
+
+        <el-table :data="comprehensiveData" style="width: 100%">
+            <el-table-column prop="a" label="学号"></el-table-column>
+            <el-table-column prop="b" label="姓名"></el-table-column>
+            <el-table-column prop="c" label="班级"></el-table-column>
+            <el-table-column prop="d" label="分数"></el-table-column>
+
+            <el-table-column label="操作">
+                <template #default="scope">
+                    <el-button size="mini" @click="goReview(scope.row)">审核</el-button>
+                </template>
+            </el-table-column>
+
+        </el-table>
+
+        <el-pagination
+            layout="total, prev, pager, next"
+            :total="totalItems"
+            :current-page.sync="currentPage"
+            :page-size="pageSize"
+            @current-change="fetchData"
+        ></el-pagination>
+
+    </div>
 </template>
 
 <script>
-
 export default {
+    data() {
+        return {
+            searchTerms: {
+                a: "",
+                b: "",
+                c: "",
+            },
+            // mock 综测信息
+            comprehensiveData: [
+                { id: 1, a: "21311111", b: "乔羿童", c: "3", d: "5.0" },
+                { id: 2, a: "21312312", b: "张三", c: "4", d: "5.0" },
+                { id: 3, a: "21342323", b: "李四", c: "1", d: "5.0" },
+                { id: 4, a: "21342323", b: "王五", c: "1", d: "5.0" },
+            ],
+            totalItems: 4,
+            pageSize: 10,
+            currentPage: 1,
+            currentItem: {},
+            isEditMode: false,
+        };
+    },
+    methods: {
+        // 根据学生信息进行搜索
+        fetchData() {
+            // 后端接口 根据 searchTerms 进行搜索
+            console.log("搜索条件", this.searchTerms);
+            // 假设这里返回了搜索结果和总数
+            // this.comprehensiveData = ...
+            // this.totalItems = ...
+        },
 
-}
+        // 跳转到当前学生的综测审核页面
+        goReview(row) {
+            this.$router.push({ path: `/compAssDetail/${row.a}`});
+        },
+    },
+};
 </script>
-
-<style>
-
-</style>
