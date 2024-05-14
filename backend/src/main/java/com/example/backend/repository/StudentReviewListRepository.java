84fac1cb42d171e9ecbb7c51c268334dd05a8f3e
bbyyj <2435928133@qq.com>
2024-05-15 00:00:42 +0800

    综测发表接口修改

--- backend/src/main/java/com/example/backend/repository/StudentReviewListRepository.java
+++ backend/src/main/java/com/example/backend/repository/StudentReviewListRepository.java
@@ -21,7 +21,7 @@ public interface StudentReviewListRepository extends JpaRepository<StudentReview
     @Modifying
     @Query("UPDATE StudentReviewList r SET r.state=1 ,r.score=?1 where r.review_id=?2 and r.student_id=?3")
     void updateScoreAndState(double score, int review_id, String student_id);
-    @Query("SELECT r FROM StudentReviewList r where r.review_id=?1 and r.isUndergraduate=?2 and r.admission_year=?3 and r.classId=?4  and r.student_id=?5 and r.sname=?6 ")
-    Page<StudentReviewList> findByReviewIdAndCondition(int review_id, boolean isUndergraduate, Year admission_year, Integer classId, String sid,String sname,Pageable  pageable);
+//    @Query("SELECT r FROM StudentReviewList r where r.review_id=?1 and r.isUndergraduate=?2 and r.admission_year=?3 and r.classId=?4  and r.student_id=?5 and r.sname=?6 ")
+//    Page<StudentReviewList> findByReviewIdAndCondition(int review_id, boolean isUndergraduate, Year admission_year, Integer classId, String sid,String sname,Pageable  pageable);
 
 }
