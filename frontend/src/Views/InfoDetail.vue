b387548161d6a06f36ec7031a25058f14eae4606
zaiyunli <jinxuan030@gmail.com>
2024-05-27 22:23:10 +0800

    资源审查-对接

--- frontend/src/Views/InfoDetail.vue
+++ frontend/src/Views/InfoDetail.vue
@@ -3,8 +3,8 @@
     <div class="competition-award">
       <div class="filter-search">
         <el-input placeholder="请输入内容" v-model="params.search" class="search-with-select" size="mini" clearable>
-          <el-select v-model="params.select" slot="prepend" placeholder="请选择" ref="select" filterable>
-            <el-option v-for="(column, index) in tableColumns" :label="column.label" :key="index" v-if="index > 0 && index < 5" :value=index></el-option>
+          <el-select v-model="selectedText" slot="prepend" placeholder="请选择" ref="select" filterable @change="handleSelectChange">
+            <el-option v-for="(column, index) in displayedOptions" :label="column.label" :key="index" :value="index"></el-option>
           </el-select>
           <el-button slot="append" icon="el-icon-search" @click="loadingData"></el-button>
         </el-input>
@@ -22,6 +22,10 @@
               <el-form-item v-for="(column, index) in tableColumns" :label="column.label" :key="index" v-if="index > 0">
                 <span>{{ props.row[column.prop]  }}</span>
               </el-form-item>
+              <el-form-item v-if="props.row.check_status === '已审核' && index === 0" v-for="(column, index) in tableColumns" :label="column.label" :key="index">
+                <span>{{ props.row[column.prop] }}</span>
+              </el-form-item>
+
             </el-form>
           </template>
         </el-table-column>
@@ -31,22 +35,22 @@
 <!--        表格里的其他项-->
         <el-table-column label="ID" prop="id"></el-table-column>
         <el-table-column label="提交时间" prop="submit_time" sortable></el-table-column>
-        <el-table-column label="审核状态" prop="check_status" sortable :sort-method="sortByState" :filters="[{text: '已审核', value:'1'}, {text: '待审核', value: '0'}]"
+        <el-table-column label="审核状态" prop="check_status" sortable :sort-method="sortByState" :filters="[{text: '已审核', value:'已审核'}, {text: '未审核', value: '未审核'}]"
                          :filter-multiple=false :filter-method="filterHandle">
-          <template slot-scope="scope">
-            {{ formatStatus(scope.row.check_status) }}
-          </template>
+<!--          <template slot-scope="scope">-->
+<!--            {{ formatStatus(scope.row.check_status) }}-->
+<!--          </template>-->
         </el-table-column>
         <el-table-column v-for="(column, index) in tableColumns" :key="index" :prop="column.prop" :label="column.label" v-if="index > 0 && index < 5"></el-table-column>
 
         <el-table-column label="操作" fixed="right">
           <template slot-scope="scope">
-            <el-popconfirm v-if="scope.row.check_status === '0'" title="该记录审核通过？" confirm-button-text='是的'
-                           cancel-button-text='存在问题，不通过' @cancel="showDialogReject(scope.row.id)" @confirm="submitCheck(scope.row.id,'1')">
+            <el-popconfirm v-if="scope.row.check_status === '未审核'" title="该记录审核通过？" confirm-button-text='是的'
+                           cancel-button-text='存在问题，不通过' @cancel="showDialogReject(scope.row.id)" @confirm="submitCheck(scope.row.id,'已审核')">
               <el-button type="info" slot="reference" size="mini" class="btn-examined" @click.native.stop>审核</el-button>
             </el-popconfirm>
 
-            <el-popconfirm v-else-if="scope.row.check_status === '1'" title="确认删除？" @confirm="deleteItems()">
+            <el-popconfirm v-else-if="scope.row.check_status === '已审核'" title="确认删除？" @confirm="deleteItems(scope.row.id)">
               <el-button type="danger" slot="reference" size="mini" class="btn-examined" @click.native.stop>删除</el-button>
             </el-popconfirm>
             <el-button @click.stop="showRichText(scope.row.url)" type="primary" size="mini" class="btn-examined">查看附件</el-button>
@@ -55,8 +59,8 @@
       </el-table>
 
 <!--      分页-->
-      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="params.pageNum"
-                     :page-sizes="[1, 2, 5, 10]" :page-size="params.pageSize" layout="total, sizes, prev, pager, next, jumper"
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pageNum"
+                     :page-sizes="[1, 2, 5, 10]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper"
                      :total="total" class="pagination">
       </el-pagination>
 
@@ -74,7 +78,7 @@
         </el-form>
         <span slot="footer" class="dialog-footer">
           <el-button @click="rejectVisible = false">取消</el-button>
-          <el-button type="primary" @click="submitCheck(rejectID,'0')">确定</el-button>
+          <el-button type="primary" @click="submitCheck(rejectID,'已审核')">确定</el-button>
         </span>
       </el-dialog>
 
@@ -93,15 +97,16 @@ export default {
   data() {
     return {
       params: {
-        pageNum: 1,   // 前往第几页
-        pageSize: 5,   // 每页显示的条数
-        search: '', // 输入的搜索内容
         select: '', // 选择的搜索项
-        class: '', // 班级id
+        search: '', // 输入的搜索内容
+        classId: '', // 班级id
         year: '', // 年份id
         isUndergraduate: true, // 是否本科
       },
       total: 0,
+
+      pageNum: 1,   // 前往第几页
+      pageSize: 5,   // 每页显示的条数
 
       tableData: [],
       textVisible: false,
@@ -111,6 +116,8 @@ export default {
       tableColumns: [],
       multipleSelection: [],
       currentImgUrl: '', // 当前预览的图片URL
+
+      selectedText:''
     }
   },
 
@@ -118,35 +125,68 @@ export default {
     this.updateTableColumns();
     this.loadingData();
   },
+  computed: {
+    displayedOptions() {  // 表头筛选项
+      let options = [];
+      let isFirst = true;
+      let count = 0;
+      Object.values(this.tableColumns).forEach((data) => {
+        if (isFirst) {
+          isFirst = false;
+          return;
+        }
+
+        let items = Object.keys(data).map(k => ({
+          label: data[k]
+        })).filter(item => /[\u4e00-\u9fa5]/.test(item.label));
+
+        if (this.$route.name === 'competition' || this.$route.name === 'paper') {
+          if(count > 3){
+            return;
+          }
+        } else if (this.$route.name === 'patent' || this.$route.name === 'software' || this.$route.name === 'monograph' || this.$route.name === 'exchange') {
+          if(count > 1){
+            return;
+          }
+        }
+        else {
+          if(count > 0){
+            return;
+          }
+        }
+        count++;
+        options = options.concat(items);
+      });
+      return options;
+    }
+  },
   watch: {
     $route(to, from) {
       this.updateTableColumns();
-      // this.loadingData();
       this.clearAll();
-
-      this.$set(this.params, 'select', null)
+      // this.loadingData();
+      this.$set(this.params, 'select', '')
 
     },
   },
   methods: {
     loadingData(){
-      // request.post('/examine/loadingdata/${this.$route.name}', {params: this.params}).then(response => {
-      axios.post('https://mock.apifox.com/m2/4212159-3852880-default/164630561', {params: this.params}).then(response => {
+      request.post(`/examine/loadingdata/${this.$route.name}`, this.params).then(response => {
+        console.log(this.params);
         if(response.data.code === 200){
-          this.tableData = response.data.data.tableData;
+          // this.tableData = response.data.data.tableData;
+          this.tableData = (response.data.data.tableData).slice((this.pageNum - 1) * this.pageSize, this.pageNum * this.pageSize);  // 分页
           this.total = response.data.data.count;
         }else{
           this.$message.error(response.data.message);
         }
       })
-
       this.tableData.forEach(val => { // 开启展开行用
         this.$set(val, "expanded", false);
       });
     },
     deleteItems(id){  // 删除
-      // request.put('/examine/${this.$route.name}/del', {id: id}).then(res => {
-      axios.put('https://mock.apifox.com/m2/4212159-3852880-default/164842747', {id: id}).then(res => {
+      request.put(`/examine/${this.$route.name}/del`, {id: id}).then(res => {
         if (res.data.code === 200) {
           this.loadingData();
           this.$message.success(res.data.message);
@@ -156,15 +196,13 @@ export default {
       })
     },
     submitCheck(id,status){ // 审核
-      // request.post("/examine/${this.$route.name}/check",{id: id,status: status,msg: this.rejectReason}).then(res => {
-      axios.post("https://mock.apifox.com/m2/4212159-3852880-default/164840260",{id: id,status: status,msg: this.rejectReason}).then(res => {
+      request.post(`/examine/${this.$route.name}/check`,{id: id,status: status,msg: this.rejectReason}).then(res => {
         if (res.data.code === 200) {
-          this.loadingData();
-          if(status === '0'){ // 不通过
-            this.rejectVisible = false;
-            this.rejectReason = "";
-          }
+          this.rejectVisible = false;
+          this.rejectReason = "";
           this.$message.success(res.data.message);
+
+          this.loadingData();
         } else {
           this.$message.error(res.data.code);
         }
@@ -184,8 +222,7 @@ export default {
         return;
       }
       // 全选全部待完善
-      this.$message.success("批量导出成功");
-
+      this.$message.success(this.multipleSelection.map(item => item.id).join(","));
 
       // request.put("/examine/delbatch", this.multipleSelection).then(res => {
       //   if (res.code === '200') {
@@ -223,12 +260,14 @@ export default {
     clearAll() {  // 清空
       this.$refs.filterTable.clearFilter();
       this.params = {
-        pageNum: 1,   // 前往第几页
-        pageSize: 5,   // 每页显示的条数
-        search: '', // 输入的搜索内容
         select: '', // 选择的搜索项
+        search: '', // 输入的搜索内容
+        classId: '', // 班级id
+        year: '', // 年份id
+        isUndergraduate: true, // 是否本科
       }
       this.rejectReason = "";
+      this.$refs.filterTable.clearSelection();
 
       this.loadingData();
     },
@@ -242,27 +281,28 @@ export default {
       this.$refs.filterTable.toggleRowExpansion(row, row.expanded);
     },
     handleSizeChange(pageSize) {  // 处理分页
-      this.params.pageSize = pageSize;
+      this.pageSize = pageSize;
+      this.pageNum = 1;
       this.loadingData();
     },
     handleCurrentChange(pageNum) {  // 处理分页
-      this.params.pageNum = pageNum;
+      this.pageNum = pageNum;
       this.loadingData();
     },
-    formatStatus(status) {
-      return status === '1' ? '已审核' : '未审核';
-    },
+    // formatStatus(status) {
+    //   return status === '1' ? '已审核' : '未审核';
+    // },
     sortByState(a, b){  // 表中审核状态排序
-      if(a.check_status === '0'){
+      if(a.check_status === '未审核'){
         return -1;
       } else{
         return 1;
       }
     },
     RowState({row, rowIndex}){  // 表中审核状态颜色
-      if(row.check_status === '0'){
+      if(row.check_status === '未审核'){
         return 'warning-row';
-      }else if(row.check_status === '1'){
+      }else if(row.check_status === '已审核'){
         return 'success-row';
       }
       return '';
@@ -279,6 +319,9 @@ export default {
     getRowKeys(row){
       return row.id;
     },
+    handleSelectChange(value) { // 获取查询对象的文本
+      this.params.select = this.tableColumns[value+1].label;
+    }
   }
 }
 </script>
