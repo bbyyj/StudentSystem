9c98fc711f1aa4ed5c38087ec8eb7df5b60401bb
YZFi777 <2671120742@qq.com>
2024-07-07 14:07:54 +0800

    学生首页

--- frontend/src/Views/studentHome.vue
+++ frontend/src/Views/studentHome.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    
+  <div class="container">
+    <img src="../assets/images/studentHome.jpg" alt="" class="bg">
   </div>
 </template>
 
@@ -11,5 +11,17 @@ export default {
 </script>
 
 <style>
-
+.container {
+display: flex;
+  justify-content: center; 
+  height: 100%; 
+  width: 100%; 
+  
+}
+.bg{
+    position: absolute;
+   height: calc(100vh - 159px);
+   width: calc(100vw - 250px);
+    
+}
 </style>
