321687f0faecc18c2db0f14040740c975a279232
YZFi777 <2671120742@qq.com>
2024-07-07 14:40:16 +0800

    删除不需要的文件

--- frontend/src/api/mock.js
+++ frontend/src/api/mock.js
@@ -1,19 +1,10 @@
 import Mock from 'mockjs'
 import homeMock from '../api/mockServe/home'
-import user from './user'
-import permission from './mockServe/permission'
 import infoTest from "@/api/mockServe/infoTest";
 
 // 定义mock拦截
 Mock.mock('/api/home/getData',homeMock)
 
-// 用户管理:增删查改
-Mock.mock(/\/api\/user\/get/,user.getUserList)
-Mock.mock('/api/user/create','post',user.createUser)
-Mock.mock('/api/user/update','post',user.updateUser)
-Mock.mock('/api/user/del','post',user.deleteUser)
 
-// 登录权限
-Mock.mock(/api\/permission\/getMenu/,'post',permission.getMenu)
 
 Mock.mock(/\/api\/infoExamine\/loadingData/,'post',infoTest.getLoadingData)
--- frontend/src/data/TableLabel.js
+++
@@ -1,8 +0,0 @@
-const TableLabel={
-    name:'课程',
-    todayBuy:'今日购买',
-    monthBuy:'本月购买',
-    totalBuy:'总购买'
-}
-
-export default TableLabel
